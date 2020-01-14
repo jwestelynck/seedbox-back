c31012094429db34997fa9c773e66e8c16beac42
Jean-Rémy Westelynck <westelynck@gmail.com>
2020-01-14 08:07:49 +0100

    fix get_size call

--- cgi-bin/getPathContent.py
+++ cgi-bin/getPathContent.py
@@ -32,7 +32,7 @@ def getPathContent(path):
                     'name':content,
                     'type':'file',
                     'last_modification': os.path.getmtime("{}/{}".format(path,content)),
-                    'size': os.path.getsize(("{}/{}".format(path,content))),
+                    'size': getsize(("{}/{}".format(path,content))),
                     'id': str(base64.b64encode(("{}/{}".format(path,content)).encode("utf-8")), "utf-8"),
                     'link': 'https://{}{}/{}'.format(hostname,path,content)
                 })
@@ -41,7 +41,7 @@ def getPathContent(path):
                     'name':content,
                     'type':'directory',
                     'last_modification': os.path.getmtime("{}/{}".format(path,content)),
-                    'size': os.path.get_size("{}/{}".format(path,content)),
+                    'size': get_size("{}/{}".format(path,content)),
                     'id': str(base64.b64encode(("{}/{}".format(path,content)).encode("utf-8")), "utf-8"),
                     'content' : getPathContent("{}/{}".format(path,content))
                 }
